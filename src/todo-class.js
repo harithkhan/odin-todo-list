70c4d41d055504e943317c8c5d03ba6bb3d19933
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-23 08:20:51 +0800

    feat: add get and set methods to class ToDo

--- src/todo-class.js
+++ src/todo-class.js
@@ -3,17 +3,66 @@ import { format } from "date-fns";
 export default class ToDo {
     constructor(title, 
         description = "N/A", 
-        duePeriod = "N/A", 
+        due = "N/A", 
         priority = 0, 
         notes = "N/A", 
         isComplete = false, 
     ) {
         this._title = title;
         this._description = description;
-        this._duePeriod = duePeriod;
+        this._due = due;
         this._priority = priority;
         this._notes = notes;
         this._isComplete = isComplete;
         this._whenCreated = format(new Date(), "h:mma, EEEE, d MMM yyyy");
+    };
+
+    get title() {
+        return this._title;
+    };
+    set title(newTitle) {
+        this._title = newTitle;
+    };
+
+    get description() {
+        return this._description;
+    };
+    set description(newDescription) {
+        this._description = newDescription;
+    };
+
+    get due() {
+        return this._due;
+    };
+    set due(newDue) {
+        this._due = newDue;
+    };
+
+    get priority() {
+        return this._priority;
+    };
+    set priority(newPriority) {
+        this._priority = newPriority;
+    };
+
+    get notes() {
+        return this._notes;
+    };
+    set notes(newNotes) {
+        this._notes = newNotes;
+    };
+
+    get isComplete() {
+        return this._isComplete;
+    };
+    set isComplete(newStatus) {
+        this._isComplete = newStatus;
+    };
+
+    get whenCreated() {
+        return this._whenCreated;
+    };
+    set whenCreated(timeStamp) {
+        this._whenCreated = timeStamp;
     };
 };
