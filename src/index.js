40279161df65433118757efd03a11faac1dcfcd6
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-24 22:26:17 +0800

    feat: add module display-hub.js Add module display-hub.js that shall contain logic to display the properties of object toDoHub in the DOM. The module is contained in a new directory "display". Add icons bin.png and edit/png to directory img.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import * as catFunction from "./logic/category-functions";
 import * as toDoFunction from "./logic/todo-functions";
 import * as checklistFunction from "./logic/checklist-functions"
 import * as addTodo from "./display/add-todo"
+
+console.log(getData());
