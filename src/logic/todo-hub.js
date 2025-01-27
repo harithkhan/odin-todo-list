5445961746a0632027b2483aa1d7677c589fada8
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-27 14:26:53 +0800

    feat: initialize trash category Add trash category to store deleted categories and to do items.

--- src/logic/todo-hub.js
+++ src/logic/todo-hub.js
@@ -3,5 +3,6 @@ import { add } from "./category-functions";
 //Initialize object that stores all instances of classes Category and ToDo 
 export let toDoHub = {}; //Only export to category-functions and todo-functions, otherwise keep private
 add("General", "General/Default category.");//Initialize General category
+add("Trash", "Container of deleted categories and to do's.");
 
 export const getData = () => toDoHub;
