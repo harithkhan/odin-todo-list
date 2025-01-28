8da256b3360e4c637d994ab9dbba125ed43f61c1
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-28 11:59:58 +0800

    feat: display all categories in cat section Add function listAllCats in module cat-display.js that appends all existing categories to the category container. Call listAllCats() in index.js.

--- src/display/cat-display.js
+++ src/display/cat-display.js
@@ -1,11 +1,29 @@
-import { getData } from "../logic/todo-hub";
+import { getData as hub} from "../logic/todo-hub";
 import { displayHub } from "./display-hub";
 
 export function listAllCats() {
+    const catContainer = document.querySelector(".category-container");
+    for (let category in hub()) {
+        if (category === "General") {
+            
+        } else if (category === "Trash") {
 
+        } else {
+            const catButton = document.createElement("button");
+            catButton.className = "cat-button";
+            catButton.type = "button";
+            catButton.textContent = category;
+            catButton.dataset.category = category;
+            catContainer.appendChild(catButton);
+        };
+    };
 };
 
 export function showAllCats() {
+
+};
+
+function displayContents() {
 
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as toDoFunction from "./logic/todo-functions";
 import * as checklistFunction from "./logic/checklist-functions"
 import * as addTodo from "./display/add-todo"
 import { displayHub } from "./display/display-hub";
+import { listAllCats } from "./display/cat-display";
 
 //Test objects
 toDoFunction.add(undefined, "5 deep breaths", "Just relax", "28/02/2025");
@@ -14,6 +15,7 @@ toDoFunction.add("ToDo List Project", "Add logic to buttons");
 
 console.log(getData());
 displayHub();
+listAllCats();
 
 const showAllButton = document.getElementById("show-all-button");
 showAllButton.addEventListener("click", displayHub);
