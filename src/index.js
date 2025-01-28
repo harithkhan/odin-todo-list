ea204775ef0fce752be8e716e0190243935a8bae
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-29 07:12:53 +0800

    style: add padding-bottom:2rem to footer;

--- src/index.js
+++ src/index.js
@@ -8,13 +8,6 @@ import { displayHub } from "./display/display-hub";
 import { buildAllCats } from "./display/cat-display";
 import { attachCatButtonListeners } from "./display/cat-display";
 
-//Test objects
-toDoFunction.add(undefined, "5 deep breaths", "Just relax", "28/02/2025");
-toDoFunction.add("Exercise", "Gym", "Do 22 sets", "29/02/2025");
-catFunction.add("ToDo List Project", "A web app that is a functional to-do list", "27/01/2025");
-toDoFunction.add("ToDo List Project", "Add logic to buttons");
-
-console.log(getData());
 displayHub();
 buildAllCats();
 attachCatButtonListeners();
--- src/logic/todo-hub.js
+++ src/logic/todo-hub.js
@@ -1,4 +1,5 @@
 import { add } from "./category-functions";
+import { add as addToDo } from "./todo-functions";
 
 //Initialize object that stores all instances of classes Category and ToDo 
 export let toDoHub = {}; //Only export to category-functions and todo-functions, otherwise keep private
@@ -7,4 +8,6 @@ export const getData = () => toDoHub;
 
 //Initialize General Category and Trash Object
 add("General", "General/Default category.");//Initialize General category
+addToDo(undefined, "Toggle the checkbox on the left");
+
 toDoHub["Trash"] = {};
