28961eacb178ae70693e3834065648e3636fdc8e
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-28 23:23:34 +0800

    feat: remove trashed cat when all todos restored Remove category from trash object and display when all todos in the concerned category is restored.

--- src/display/restore.js
+++ src/display/restore.js
@@ -1,5 +1,6 @@
 import { restore as restoreCat } from "../logic/category-functions";
 import { restore as restoreToDo } from "../logic/todo-functions";
+import { getData } from "../logic/todo-hub";
 
 export function handleRestoreCatClick(event) {
     const restoreButton = event.target.closest(".cat-restore-button");
@@ -13,9 +14,16 @@ export function handleRestoreCatClick(event) {
 
 export function handleRestoreToDoClick(event) {
     const title = event.target.dataset.title;
+    const category = event.target.dataset.category;
     restoreToDo(title);
     const toRestore = document.querySelectorAll(`.todo-item[data-title="${title}"]`);
     toRestore.forEach((element) => {
         element.remove();
     });
+    const testObj = getData().Trash[category].toDo;
+    if (Object.keys(testObj).length === 0) {
+        delete getData().Trash[category];
+        const categoryHeader = document.querySelector(`.todo-item[data-category="${category}"]`);
+        categoryHeader.remove();
+    };
 };
